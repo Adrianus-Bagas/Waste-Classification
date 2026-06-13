b9a0f6b16e3e00b54925aebd6f2b278380dc902f
Adrianus-Bagas <bagasadrianus80@gmail.com>
2026-06-13 15:02:01 +0700

    ci: add deploy yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/Waste-Classification",
+  assetPrefix: "/Waste-Classification",
+  images: {
+    unoptimized: true,
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
